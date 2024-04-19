dcbe92fe67ad4f1e7f8036f5f5da53d789f3a422
Etherisium <zarin.sumaiya@gmail.com>
2024-04-20 05:52:17 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,34 @@
+function connect() {
+
+    var searchanime = document.getElementById("animebox").value ;
+
+   
+
+    var url= `https://kitsu.io/api/edge/anime?filter[text]=${searchanime}`;
+
+    fetch (url)
+    .then (res => res.json() )
+    .then( animedata => {console.log(animedata); show(animedata);})  }
+    
+function show (animedata) {
+    console.log(animedata)
+
+        var oldContent = document.getElementById("container2");
+    
+       oldContent.textContent = " "; 
+       
+        for (var i=0; i< animedata.length; i++)
+        
+        {
+       
+           var newDiv = document.createElement("div");
+    
+           newDiv.innerHTML = `<h3> Anime Name: <b>${animedata.data[i].attributes.canonicalTitle}</b> </h3><br>
+                <img src= "${animedata.data[i].attributes.coverimage.large}"> <br> <br>`;
+    
+           newDiv.classList.add("innerStyle");
+    
+           oldContent.appendChild(newDiv); 
+           
+        }
+    }
